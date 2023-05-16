d9723a40f94301dd4957838c5f856782544536ca
luiz-august0 <66564172+luiz-august0@users.noreply.github.com>
2023-05-15 21:31:34 -0300

    Implementação de tiro

--- Assets/charScript.cs
+++ Assets/charScript.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class charScript : MonoBehaviour
 {
+    public GameObject fireProject;
+    public Transform gun;
+    private bool fire;
+    public float fireForce;
+    private bool flipX = false;
     public float Speed;
     public float JumpForce;
     public bool isJumping;
@@ -17,6 +23,8 @@ public class charScript : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+        fire = Input.GetButtonDown("Fire1");
+        Fire();
         Move();
         Jump();
     }
@@ -27,12 +35,14 @@ public class charScript : MonoBehaviour
 
         float inputAxis = Input.GetAxis("Horizontal");
 
-        if (inputAxis > 0) {
-            transform.eulerAngles = new Vector2(0f, 0f);
+        if (flipX == true && inputAxis > 0) {
+            Flip();
+            //transform.eulerAngles = new Vector2(0f, 0f);
         }
 
-        if (inputAxis < 0) {
-            transform.eulerAngles = new Vector2(0f, 180f);
+        if (flipX == false && inputAxis < 0) {
+            Flip();
+            //transform.eulerAngles = new Vector2(0f, 180f);
         }
     }
 
@@ -40,5 +50,22 @@ public class charScript : MonoBehaviour
         if (Input.GetButtonDown("Jump") && !isJumping) {
             rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
         }
+    }
+
+    private void Fire() {
+        if (fire == true) {
+            GameObject temp = Instantiate(fireProject);
+            temp.transform.position = gun.position;
+            temp.GetComponent<Rigidbody2D>().velocity = new Vector2(fireForce, 0);
+            Destroy(temp.gameObject, 3f);
+        }
+    }
+
+    private void Flip() {
+        flipX = !flipX;
+        float x = transform.localScale.x;
+        x *= -1;
+        transform.localScale = new Vector3(x, transform.localScale.y, transform.localScale.z);
+        fireForce *= -1;
     }
 }
