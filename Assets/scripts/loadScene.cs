7ce29250084ae00c5728f335cfcc9a43e4887744
luiz-august0 <66564172+luiz-august0@users.noreply.github.com>
2023-05-22 21:41:13 -0300

    Implementado post na unity

---
+++ Assets/scripts/loadScene.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
+using System.Threading.Tasks;
+
+public class UserData  {
+    public string player;
+    public double score;
+}
+
+public class loadScene : MonoBehaviour
+{
+    private async Task SendScore() {
+        String username = userData.userName;
+        username = username.Replace("\"", "\\\"");;
+
+        var user = new UserData();
+        user.player = username;
+        user.score = 1000;
+
+        string json = JsonUtility.ToJson(user);
+
+        var uwr = new UnityWebRequest("https://api-gameconstruct3.vercel.app/player_score", "POST");
+        byte[] jsonToSend = new System.Text.UTF8Encoding().GetBytes(json);
+        uwr.uploadHandler = (UploadHandler)new UploadHandlerRaw(jsonToSend);
+        uwr.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
+        uwr.SetRequestHeader("Content-Type", "application/json");
+        
+        uwr.SendWebRequest();
+
+        if (uwr.isNetworkError) {
+            Debug.Log(uwr.error);
+        } else {
+            Debug.Log("Sended");
+        }
+    }
+
+    userData userData;
+    public GameObject inputName;
+    public async void LoadScene() {
+        userData.userName = inputName.GetComponent<Text>().text;
+        await SendScore();
+        SceneManager.LoadScene("game");
+    }
+}
